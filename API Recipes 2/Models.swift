b983b0b4e7a68a9dd9ae981ed695eab47c6bbdde
Maya Krishnan <mayark527@gmail.com>
2026-05-08 10:38:42 -0500

    Added Models file from old API app

---
+++ API Recipes 2/Models.swift
@@ -0,0 +1,20 @@
+//
+//  Models.swift
+//  API Recipes 2
+//
+//  Created by Maya Krishnan on 5/8/26.
+//
+
+import Foundation
+
+struct RecipeResponse: Codable {
+    let recipes: [Recipe]
+}
+
+struct Recipe: Identifiable, Codable {
+    let id: Int
+    let name: String
+    let ingredients: [String]
+    let instructions: [String]
+    let image: String
+}
