52874270513323bbb7482d48cd6567b0a0edd15f
Maya Krishnan <mayark527@gmail.com>
2026-05-08 10:40:46 -0500

    Updated ContentView to match old API app

--- API Recipes 2/ContentView.swift
+++ API Recipes 2/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var recipes = [Recipe]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(recipes) { recipe in
+                NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                HStack {
+                    AsyncImage(url: URL(string: recipe.image)) { phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        } else if phase.error != nil {
+                            Color.red // shows if image fails
+                        } else {
+                            ProgressView() // loaing spinner
+                        }
+                    }
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(8)
+                }
+            }
+            }
+                .navigationTitle("Recipes")
+            //.navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .task {
+            await loadRecipes()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(
+                title: Text("Loading Error"),
+                message: Text("Could not load recipes."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+    }
+    
+    func loadRecipes() async {
+        let link = "https://dummyjson.com/recipes"
+        if let url = URL(string: link) {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode(RecipeResponse.self, from: data) {
+                    recipes = decodedResponse.recipes
+                }
+            }
+        }
+        showingAlert = true
     }
 }
 
